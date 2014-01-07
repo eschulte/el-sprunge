37855ec60aeb4d565c49a4d711edc7341e9a22cb
Eric Schulte <schulte.eric@gmail.com>
2014-01-06 18:39:26 -0700

    outfitted for packaging via MELPA

---
+++ el-sprunge-pkg.el
@@ -0,0 +1,3 @@
+(define-package "el-sprunge" (format-time-string "%Y%m%d.%H%M")
+  "Command line paste server with Emacs highlighting"
+  '((web-server "20140105.2246") (htmlize "20130207.1202") (emacs "24.3")))
